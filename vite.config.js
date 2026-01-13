c8809c9c2e6beb726c8afed88a3e8f5b67011712
Mienzzz <icemintblack@gmail.com>
2026-01-13 14:15:49 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,48 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: 'generateSW',
+      registerType: 'autoUpdate',
+
+      includeAssets: [
+        'icon-192.png',
+        'icon-512.png'
+      ],
+
+      workbox: {
+        navigateFallback: '/index.html',
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+
+      manifest: {
+        name: 'Loan Tracker',
+        short_name: 'Loan Tracker',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#1e3a8a',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
 
