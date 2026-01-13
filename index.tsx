bd27b53253ff9cd12dec5cd7e5d90ac737f8803d
Mienzzz <icemintblack@gmail.com>
2026-01-13 13:18:20 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,6 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({ immediate: true });
+
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
